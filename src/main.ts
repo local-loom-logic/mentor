39884de2d72cd780053c47b90af8e9c2c952d950
Local Loom Logic <local.loom.logic@gmail.com>
2026-02-23 16:46:43 -0500

    Explanation of motivation and words of caution

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { updateStorageDisplay, checkPresence, fetchFullGemma, deleteGemma } from
 import { clickBubble1 } from "./chat";
 import { enableGemma } from "./model-details";
 
+const testing = false;
+
 // js/main.js
 console.log("LLLM: Logic initialized.");
 
@@ -28,5 +30,10 @@ function setupUIListenerButtons() {
 
     const bubble1 = document.getElementById('thought-bubble-1') as HTMLDivElement;
     bubble1?.addEventListener('click', clickBubble1, { once: true });
+
+    if (testing) {
+        const bubble2 = document.getElementById('thought-bubble-2') as HTMLDivElement;
+        bubble2.style.display = "block"
+    }
 
 }
